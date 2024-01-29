58d79c22479698a6117bc7a80b365734522c8959
WIll Stansill <willstansill@gmail.com>
2024-01-29 12:02:08 -0500

    Rough Draft

--- vector-extraction/src/App.js
+++ vector-extraction/src/App.js
@@ -1,5 +1,21 @@
 import logo from './logo.svg';
+// import {fetchDataAndAggregate } from 'moon-sdk-main/my-app/src/pages/usemoonsdk.tsx';
 import './App.css';
+import { MoonApi } from '@moonup/moon-api';
+import { BigNumber } from '@ethersproject/bignumber';
+
+const moonApi = new MoonApi({
+  Auth: {
+    securityWorker: () => Promise.resolve('test'),
+  },
+});
+
+async function main() {
+  const account = await moonApi.getAccount('0xB0739AaF97B5F12D3529ec6C109fbE1A9c9F6bAe');
+  console.log(account);
+}
+
+main();
 
 function App() {
   return (
