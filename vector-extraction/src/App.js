d836e996fd46b83f900b182cd1f63e9eac382eaa
WIll Stansill <willstansill@gmail.com>
2024-01-29 12:47:28 -0500

    Next

--- vector-extraction/src/App.js
+++ vector-extraction/src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Auth } from '@moonup/moon-api';
 import { Accounts } from '@moonup/moon-api';
 import { BigNumber } from '@ethersproject/bignumber';
 
-const moonApi = new Accounts({
+const moonApi = new Auth({
   Auth: {
     securityWorker: () => Promise.resolve('test'),
   },
 });
 
 async function main() {
-  const account = await moonApi.getAccount('0xB0739AaF97B5F12D3529ec6C109fbE1A9c9F6bAe');
+  const account = await Accounts.getAccount('0xB0739AaF97B5F12D3529ec6C109fbE1A9c9F6bAe');
+
   console.log(account);
 }
 
